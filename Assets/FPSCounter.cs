4f4d17f43c346033a165f2be5a93f29bcea94ca8
jake <jake@jakecoffman.com>
2018-05-03 17:06:08 -0500

    init

---
+++ Assets/FPSCounter.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSCounter : MonoBehaviour {
+
+    public int FPS { get; private set; }
+
+    int[] fpses;
+    int fpsesIndex;
+
+	private void Awake()
+	{
+        FPS = 60;
+        fpses = new int[10];
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+    void Update()
+    {
+        fpses[fpsesIndex++] = (int)(1f / Time.unscaledDeltaTime);
+        if (fpsesIndex > fpses.Length-1) {
+            fpsesIndex = 0;
+        }
+        int sum = 0;
+        for (int i = 0; i < fpses.Length; i++) {
+            sum += fpses[i];
+        }
+        FPS = sum / fpses.Length;
+    }
+}
---
+++ Assets/Nucleon.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Nucleon : MonoBehaviour {
+    public float attractionForce;
+    Rigidbody body;
+
+	private void Awake()
+	{
+        body = GetComponent<Rigidbody>();
+	}
+
+	private void FixedUpdate()
+	{
+        body.AddForce(transform.localPosition * -attractionForce);
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
